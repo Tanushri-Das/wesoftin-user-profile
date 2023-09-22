a5e003e64d2e9dcb8f309ea48e145b6192fad045
tanu <dastanushri402@gmail.com>
2023-09-22 12:40:23 +0600

    update home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="my-20">
-      <h2 className="text-3xl font-semibold text-center mb-5">
+      <h2 className="text-3xl font-semibold text-center mb-6">
         Welcome to my website
       </h2>
-      <p className="text-lg md:text-xl font-medium text-center mb-10">
+      <p className="text-lg md:text-xl font-medium text-center mb-10 mx-3 sm:mx-0">
         Do you want to see the users of my website ? Kindly Click this button
       </p>
       <div class="flex text-center justify-center">
